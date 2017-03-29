e4523ac6b7b3423ec3a8d345f2a7e9d77b2693f1
bradley-benjamin26 <bradley.benjamin26@gmail.com>
2017-03-29 18:51:13 -0400

    Add files via upload

---
+++ Chapter+3.py
@@ -0,0 +1,105 @@
+
+# coding: utf-8
+
+# In[15]:
+
+def double(sequence):
+    result = []
+    for element in sequence:
+        result = result + [element * 2]
+    return result
+
+
+# In[2]:
+
+double([1, 10, 5])
+
+
+# In[3]:
+
+double([-42, 0, 42])
+
+
+# In[4]:
+
+double([23])
+
+
+# In[5]:
+
+double([])
+
+
+# In[6]:
+
+double()
+
+
+# In[7]:
+
+double([1, 10, 5])
+
+
+# In[8]:
+
+double([1, 10, 5])
+
+
+# In[9]:
+
+double([1, 10, 5])
+
+
+# In[10]:
+
+double([1, 10, 5])
+
+
+# In[11]:
+
+double([1, 10, 5])
+
+
+# In[12]:
+
+double([1, 10, 5])
+
+
+# In[14]:
+
+double([1, 10, 5])
+
+
+# In[16]:
+
+double([1, 10, 5])
+
+
+# In[17]:
+
+x = [1, 2, 3]
+
+
+# In[18]:
+
+double(x)
+
+
+# In[19]:
+
+x
+
+
+# In[21]:
+
+double([1, 2], [3, 4])
+
+
+# In[ ]:
+
+def halver(sequence):
+    result = []
+    for element in sequence:
+        result = result + [element / 2]
+    return result
+
